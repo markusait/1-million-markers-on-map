15a95f3368d92d0b8b34e1a0afcbaa62b9815f65
markusait <markus.saitmacher@gmail.com>
2024-05-31 14:07:52 -0700

    Increased points to 1 million

--- pages/api/lats-longs-clusters.js
+++ pages/api/lats-longs-clusters.js
@@ -22,7 +22,8 @@ const convertClustersToGeoJSON = (clusters) => {
 };
 
 const initializeIndex = () => {
-  points = generateRandomLatLongPoints(500000); // Adjust the number of points based on your performance needs
+  const numberOfPoints = 1_000_000;
+  points = generateRandomLatLongPoints(numberOfPoints); 
   index = new Supercluster({ radius: 40, maxZoom: 16 });
   index.load(points);
 };
